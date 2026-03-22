e792595e4a849efd97ebd1c06707e6ae9cae78e4
Mister Chicken Admin <admin@chickenmaster.com>
2026-03-22 23:34:09 +0200

    lockal11111

--- backend/server.js
+++ backend/server.js
@@ -35,12 +35,6 @@ app.use(cors({
     const allowedOrigins = [
       'http://localhost:8555',
       'http://127.0.0.1:8555',
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-      'http://localhost:5500',
-      'http://127.0.0.1:5500',
-      'http://localhost:58767',
-      'http://127.0.0.1:58767',
       'http://localhost:5173',
       'http://127.0.0.1:5173',
       ...envOrigins
