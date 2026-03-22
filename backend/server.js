ec7320abd19beff9a4d2357f821b30a778390142
Mister Chicken Admin <admin@chickenmaster.com>
2026-03-23 00:05:01 +0200

    lockal11111

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
